7580b2ab2c2eae4b7e6fe5b602ce866439bb9146
Alberto Rocha <betogrun@gmail.com>
2017-11-05 17:50:30 -0200

    Add model for form entity

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,3 +1,11 @@
 class Form < ApplicationRecord
   belongs_to :user
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy  
+  
+  validates :title, :description, :user, presence: true
+  validates :title, uniqueness: true
+  
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
