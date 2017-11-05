b33694399c0dcde2c7c93e7129432d34fa5cf5ac
Alberto Rocha <betogrun@gmail.com>
2017-11-05 18:16:01 -0200

    Add form controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,12 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Custom json helpers
-config.include Requests::JsonHelpers, type: :request
-# Custom Header helpers
-config.include Requests::HeaderHelpers, type: :request
 
-config.include Devise::Test::ControllerHelpers, type: :controller
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,4 +56,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # Custom json helpers
+  config.include Requests::JsonHelpers, type: :request
+  # Custom Header helpers
+  config.include Requests::HeaderHelpers, type: :request
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
